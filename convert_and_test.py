dda675af7719bc191487c4fa0179cba54297b161
Rudy <rudy.dev.kor@gmail.com>
2021-08-17 17:59:34 +0900

    Update convert_and_test module

--- convert_and_test.py
+++ convert_and_test.py
@@ -1,10 +1,24 @@
+import glob
+import os
 
 from model_utils.export_tflite_graph import export_tflite_graph
-import train.config
-config = train.config.config
 
-model_name = ''
-checkpoint_name = ''
+from convert_tflite.main import convert_tflite_int8
+from train.input_pipeline import make_tfdataset
+
+from inference.inference_tflite import InferenceModel, image_inference
+
+model_name = 'MobileNetV2_custom_v3_SSD_text_seg'
+checkpoint_name = 'best-5'
+
+saved_model_path = f'./checkpoints/{model_name}/saved_model/'
+img_size_wh = [128,64]
+dataset = 'digit_train.tfrecord'
+quant_level = 2
+tflite_path = f'{model_name}_{checkpoint_name}.tflite'
+
+test_image_dir = 'inference/test_images'
+result_image_dir = 'inference/result_images'
 
 # 1.create saved model
 meta_info_path = './checkpoints/{}'.format(model_name)
@@ -12,7 +26,18 @@ export_tflite_graph(meta_info_path+'/meta_info.config', meta_info_path,
                     checkpoint_name=checkpoint_name)
 
 # 2. convert to tflite
- 
+dataset = make_tfdataset(dataset, 1, img_size_wh, False)
+convert_tflite_int8(saved_model_path, dataset, tflite_path, quant_level)
 
-                
+# 3. test images with tflite
+exts = ['*.jpg', '*.jpeg', '*.png']
+img_paths = []
+for ext in exts:
+    img_paths.extend(glob.glob(os.path.join(test_image_dir, ext)))
+os.makedirs(result_image_dir, exist_ok=True)
 
+print(img_paths)
+model = InferenceModel(tflite_path)
+for img_path in img_paths:
+    img_name = os.path.basename(img_path)
+    image_inference(model, img_path, os.path.join(result_image_dir, img_name))
