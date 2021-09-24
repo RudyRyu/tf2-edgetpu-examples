665c417a672c55d51d242f63ffb10512404c22fe
Rudy <rudy.dev.kor@gmail.com>
2021-09-24 11:26:17 +0900

    Update config

--- train/config.py
+++ train/config.py
@@ -3,7 +3,7 @@
 # for text detection
 #
 config = {
-    'model_name': 'MobileNetV2_custom_v3_SSD_text_seg',
+    'model_name': 'ShDigit_v1_SSD_text_seg',
 
     'checkpoint': '',
 
@@ -30,13 +30,13 @@ config = {
         # If an empty string, it is built based on 'model_type'.
         # It is used for a custom feature extractor.
         #
-        'feature_extractor': 'ssd_textnet_keras',
+        'feature_extractor': 'ssd_digit_sh_keras',
 
         'matched_threshold': 0.5,
         'unmatched_threshold': 0.5,
         'base_anchor_height': 1.0,
         'base_anchor_width': 1.0,
-        'num_layers': 6,
+        'num_layers': 2,
 
         # bifpn is only for efficient det arch.
         'bifpn':{
@@ -62,64 +62,64 @@ config = {
 }
 
 
-#
-# for license plate detection
-#
-config__ = {
-    'model_name': 'MobileNetV2_license_SSD_640x640',
+# #
+# # for license plate detection
+# #
+# config__ = {
+#     'model_name': 'MobileNetV2_license_SSD_640x640',
 
-    'checkpoint': '',#'checkpoints/EfficientDet_D0_SSD_640x640_lp',
+#     'checkpoint': '',#'checkpoints/EfficientDet_D0_SSD_640x640_lp',
 
-    'batch_size' : 16,
-    'num_grad_accum': 16,
-    'epoch' : 50,
-    #
-    # Shape order is [Height, Width, Channel].
-    #
-    'input_shape' : [640, 640, 3],
-    'num_classes': 1,
+#     'batch_size' : 16,
+#     'num_grad_accum': 16,
+#     'epoch' : 50,
+#     #
+#     # Shape order is [Height, Width, Channel].
+#     #
+#     'input_shape' : [640, 640, 3],
+#     'num_classes': 1,
 
-    #
-    # Choose one of below:
-    # 1. MobileNetV2_SSD
-    # 2. MobileNetV2_FPN_SSD
-    # 3. EfficientDet_D0_SSD
-    # 4. ResNet50V1_FPN_SSD
-    #
-    'model_type': 'MobileNetV2_SSD',
+#     #
+#     # Choose one of below:
+#     # 1. MobileNetV2_SSD
+#     # 2. MobileNetV2_FPN_SSD
+#     # 3. EfficientDet_D0_SSD
+#     # 4. ResNet50V1_FPN_SSD
+#     #
+#     'model_type': 'MobileNetV2_SSD',
 
-    'meta_info':{
-        #
-        # If an empty string, it is built based on 'model_type'.
-        # It is used for a custom feature extractor.
-        #
-        'feature_extractor': '',
+#     'meta_info':{
+#         #
+#         # If an empty string, it is built based on 'model_type'.
+#         # It is used for a custom feature extractor.
+#         #
+#         'feature_extractor': '',
 
-        'matched_threshold': 0.5,
-        'unmatched_threshold': 0.5,
-        'base_anchor_height': 1.0,
-        'base_anchor_width': 1.0,
-        'num_layers': 6,
+#         'matched_threshold': 0.5,
+#         'unmatched_threshold': 0.5,
+#         'base_anchor_height': 1.0,
+#         'base_anchor_width': 1.0,
+#         'num_layers': 6,
 
-        # bifpn is only for efficient det arch.
-        'bifpn':{
-            'num_iterations': 3,
-            'num_filters': 64
-        },
-    },
+#         # bifpn is only for efficient det arch.
+#         'bifpn':{
+#             'num_iterations': 3,
+#             'num_filters': 64
+#         },
+#     },
 
-    #
-    # Choose one of below:
-    #  1. Adam
-    #  2. SGD with momentum=0.9 and nesterov=True
-    #
-    'optimizer' : 'Adam',
+#     #
+#     # Choose one of below:
+#     #  1. Adam
+#     #  2. SGD with momentum=0.9 and nesterov=True
+#     #
+#     'optimizer' : 'Adam',
 
-    #
-    # initial learning rate.
-    #
-    'learning_rate' : 1e-4,
+#     #
+#     # initial learning rate.
+#     #
+#     'learning_rate' : 1e-4,
 
-    'train_file': 'lp_train.tfrecord',
-    'test_file': 'lp_valid.tfrecord',
-}
+#     'train_file': 'lp_train.tfrecord',
+#     'test_file': 'lp_valid.tfrecord',
+# }
