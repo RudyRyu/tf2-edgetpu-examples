e8f5f8283f991f0c3e4e82ea1ba370f3230a4974
Rudy <rudy.dev.kor@gmail.com>
2021-09-27 18:02:52 +0900

    Update tf dataset input pipeline
     - Not tested yet

--- train/input_pipeline.py
+++ train/input_pipeline.py
@@ -1,36 +1,15 @@
 import tensorflow as tf
 import tensorflow_addons as tfa
+import imgaug as ia
+import imgaug.augmenters as iaa
+from imgaug.augmentables.bbs import BoundingBox, BoundingBoxesOnImage
 
+import augmentation as aug
 
 TF_AUTOTUNE = tf.data.AUTOTUNE
 
-
-def random_flip(x: tf.Tensor):
-    return tf.image.random_flip_left_right(x)
-
-def gray(x):
-        return tf.image.grayscale_to_rgb(tf.image.rgb_to_grayscale(x))
-
-def random_color(x: tf.Tensor):
-    x = tf.image.random_hue(x, 0.2)
-    x = tf.image.random_brightness(x, 0.2)
-    x = tf.image.random_contrast(x, 0.8, 1.2)
-    return x
-
-def blur(x):
-    choice = tf.random.uniform([], 0, 1, dtype=tf.float32)
-    def gfilter(x):
-        return tfa.image.gaussian_filter2d(x, [5, 5], 1.0, 'REFLECT', 0)
-
-
-    def mfilter(x):
-        return tfa.image.median_filter2d(x, [5, 5], 'REFLECT', 0)
-
-
-    return tf.cond(choice > 0.5, lambda: gfilter(x), lambda: mfilter(x))
-
-def make_tfdataset(tfrecord_path, batch_size, img_shape, enable_aug=False):
-    ds = tf.data.TFRecordDataset(tfrecord_path)
+def generate_tfdataset(tfrecord_path, batch_size, img_shape,
+                       augmentation_list=False, normalization=False):
 
     def _read_tfrecord(serialized):
         description = {
@@ -51,40 +30,24 @@ def make_tfdataset(tfrecord_path, batch_size, img_shape, enable_aug=False):
         image = tf.image.resize(image, img_shape)
         return image, label, box
 
-    def map_eager_decorator(func):
-        def wrapper(images, labels, boxes):
-            return tf.py_function(
-                func,
-                inp=(images, labels, boxes),
-                Tout=(images.dtype, labels.dtype, boxes.dtype)
-            )
-        return wrapper
+    def _preprocess_image(image, label_list, box_list,
+                          augmentation_list=False, normalization=False):
+        image = (2.0 / 255.0) * image - 1.0
+        return image, label_list, box_list
 
-    def _preprocess(imgs, labels, boxes):
-        imgs = (2.0 / 255.0) * imgs - 1.0
-        labels = tf.one_hot(tf.cast(labels, tf.int32), 1)
-        labels = tf.cast(labels, tf.float32)
-        labels = [tf.expand_dims(i, 0) for i in labels]
-        boxes = [tf.expand_dims(i, 0) for i in boxes]
-        return imgs, labels, boxes
+    ds = tf.data.TFRecordDataset(tfrecord_path)
 
-    def _preprocess_images(imgs, labels, boxes):
-        imgs = (2.0 / 255.0) * imgs - 1.0
-        # imgs = imgs / 255.0
-        return imgs, labels, boxes
-
-    ds = ds.shuffle(10000)
-    ds = ds.map(_read_tfrecord)
-    ds = ds.apply(tf.data.experimental.dense_to_ragged_batch(batch_size=batch_size))
-    # ds = ds.map(map_eager_decorator(_preprocess))
-    ds = ds.map(_preprocess_images)
-    if enable_aug:
-        augmentations = [random_color, gray, blur]
-        for f in augmentations:
-            choice = tf.random.uniform([], 0.0, 1.0)
-            ds = ds.map(lambda img, label, box: (tf.cond(choice > 0.5, lambda: f(img), lambda: img), label, box),
-                num_parallel_calls=TF_AUTOTUNE)
-        ds = ds.map(lambda img, label, box: (tf.clip_by_value(img, 0., 1.), label, box), num_parallel_calls=TF_AUTOTUNE)
-    ds = ds.prefetch(TF_AUTOTUNE)
+    ds = (
+        ds
+        .map(_read_tfrecord, num_parallel_calls=TF_AUTOTUNE)
+        .cache()
+        .shuffle(buffer_size=tf.data.experimental.cardinality(ds).numpy())
+        .map(lambda image, label_list, box_list: _preprocess_image(
+            image, label_list, box_list,
+            augmentation_list=augmentation_list, 
+            normalization=normalization
+        ), num_parallel_calls=TF_AUTOTUNE)
+        .prefetch(TF_AUTOTUNE)
+    )
 
     return ds
